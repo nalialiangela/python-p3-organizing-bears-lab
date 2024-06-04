735089e3fd770192826a1b5324eef29ff3d7604e
naliali <nalialiangela365@gmail.com>
2024-06-04 14:34:42 +0300

    initial commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 10, 'M', 'Brown', 'Sweet', 1),
+(2, 'Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+(3, 'Tabitha', 4, 'F', 'Golden', 'Playful', 1),
+(4, 'Sergeant Brown', 9, 'M', 'Brown', 'Stern', 1),
+(5, 'Melissa', 5, 'F', 'White', 'Gentle', 1),
+(6, 'Grinch', 8, 'M', 'Green', 'Grumpy', 1),
+(7, 'Wendy', 6, 'F', 'Brown', 'Curious', 1),
+(8, NULL, 3, 'F', 'Black', 'Timid', 1);
